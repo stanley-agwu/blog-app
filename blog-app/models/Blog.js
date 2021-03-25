909dcf41dbd79ab691beedd0330461c7abba69f4
stanleyagwu2016@gmail.com <stanleyagwu2016@gmail.com>
2021-03-25 09:28:57 +0100

    created a model

---
+++ blog-app/models/Blog.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose")
+
+const BlogSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    markdown: {
+        type: String,
+        required: true
+    }
+
+})
+
+module.exports = mongoose.model("Blog", BlogSchema)
--- blog-app/routes/index.js
+++ blog-app/routes/index.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const router = express.Router()
+const Blog = require('../models/Blog')
 
 const blogs = require("./blogsData")
 
