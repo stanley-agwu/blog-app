f978caa0646256a548a74c073d7704feb5d5d7d2
stanleyagwu2016@gmail.com <stanleyagwu2016@gmail.com>
2021-03-26 11:00:05 +0100

    added slugify

--- blog-app/models/Blog.js
+++ blog-app/models/Blog.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose")
+const marked = require('marked')
+const slugify = require('slugify')
 
 const BlogSchema = new mongoose.Schema({
     title: {
@@ -13,7 +15,18 @@ const BlogSchema = new mongoose.Schema({
     },
     markdown: {
         type: String
+    },
+    slug: {
+        type: String,
+        unique: true
     }
+})
+
+BlogSchema.pre("validate", function(next){
+    if (this.title){
+        this.slug = slugify(this.title, { lower: true, strict: true })
+    }
+    next()
 })
 
 module.exports = mongoose.model("Blog", BlogSchema)
--- blog-app/routes/index.js
+++ blog-app/routes/index.js
@@ -11,14 +11,14 @@ router.get("/", async (req, res) => {
 router.get("/new", (req, res) =>{
     res.render("blogs/new", { blog: new Blog()})
 })
-router.get("/blog/:id", async (req, res) =>{
-    let blog = await Blog.findById(req.params.id)
+router.get("/blog/:slug", async (req, res) =>{
+    let blog = await Blog.findOne({ slug: req.params.slug })
     console.log(req.params)
     if (blog === null || !blog) res.redirect("/")
     res.render("blogs/blog", { blog: blog} )
 })
-router.get("/edit/:id", async (req, res) =>{
-    let blog = await Blog.findById(req.params.id)
+router.get("/edit/:slug", async (req, res) =>{
+    let blog = await Blog.findById(req.params.slug)
     res.render("blogs/edit", { blog: blog} )
 })
 router.post("/", async (req, res) => {
@@ -31,11 +31,12 @@ router.post("/", async (req, res) => {
     try {
         blog = await blog.save()
         console.log(blog)
-        res.redirect(`/blog/${blog.id}`)
+        res.redirect(`/blog/${blog.slug}`)
     } catch (error) {
         console.error(error)
         res.redirect("/")
     }
 })
+
 
 module.exports = router
