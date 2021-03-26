aedd866f040df11b947d7d47864995cf1314a530
stanleyagwu2016@gmail.com <stanleyagwu2016@gmail.com>
2021-03-26 17:37:32 +0100

    sanitized the markdown

--- blog-app/models/Blog.js
+++ blog-app/models/Blog.js
@@ -1,6 +1,12 @@
 const mongoose = require("mongoose")
 const marked = require('marked')
 const slugify = require('slugify')
+const createDomPurifier = require("dompurify")
+const { JSDOM } = require("jsdom")
+
+const window = new JSDOM("").window
+const DOMPurifier = createDomPurifier(window)
+
 
 const BlogSchema = new mongoose.Schema({
     title: {
@@ -19,6 +25,9 @@ const BlogSchema = new mongoose.Schema({
     slug: {
         type: String,
         unique: true
+    },
+    sanitizedHtml: {
+        type: String,
     }
 })
 
@@ -26,7 +35,11 @@ BlogSchema.pre("validate", function(next){
     if (this.title){
         this.slug = slugify(this.title, { lower: true, strict: true })
     }
+    if (this.markdown){
+        this.sanitizedHtml = DOMPurifier.sanitize(marked(this.markdown))
+    }
     next()
 })
+
 
 module.exports = mongoose.model("Blog", BlogSchema)
